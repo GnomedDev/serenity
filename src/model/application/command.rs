54199a35c103bc48146682f51fc37d9c4f67185f
Gnome! <david2005thomas@Gmail.com>
2024-01-29 18:45:59 +0000

    Convert enum number into wrapper struct to save type sizes (#2746)
    Saves a couple bytes in each model by replacing the enums with a simpler
    and more accurate representation, a newtype with named statics.
    https://www.diffchecker.com/ohzayL9Z/

--- src/model/application/command.rs
+++ src/model/application/command.rs
@@ -225,7 +225,6 @@ enum_number! {
     /// [Discord docs](https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types).
     #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
     #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
-    #[serde(from = "u8", into = "u8")]
     #[non_exhaustive]
     pub enum CommandType {
         ChatInput = 1,
@@ -301,7 +300,6 @@ enum_number! {
     /// [Discord docs](https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type).
     #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
     #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
-    #[serde(from = "u8", into = "u8")]
     #[non_exhaustive]
     pub enum CommandOptionType {
         SubCommand = 1,
@@ -374,7 +372,6 @@ enum_number! {
     /// [Discord docs](https://discord.com/developers/docs/interactions/application-commands#application-command-permissions-object-application-command-permission-type).
     #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
     #[cfg_attr(feature = "typesize", derive(typesize::derive::TypeSize))]
-    #[serde(from = "u8", into = "u8")]
     #[non_exhaustive]
     pub enum CommandPermissionType {
         Role = 1,
